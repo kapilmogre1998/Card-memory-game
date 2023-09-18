4255424af54cf2d26ca1815610c56b69cd8a9116
kapilmogre1998 <kapil.mogre@nobroker.in>
2023-09-18 14:58:42 +0530

    added character imgs and background img

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { uniqueCards } from './constant';
 import './App.css'
 
 const App = () => {
+  const [cardList, setCardList] = useState([]);
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState([]);
 
   const shuffleCards = (list) => {
     const length = list.length;
@@ -14,15 +17,51 @@ const App = () => {
       list[currentIndex] = list[randomIndex];
       list[randomIndex] = temp;
     }
-    return list;
+    setCardList(list)
   }
+
+  const matchCards = () => {
+    const [first, second] = openCards;
+    if (cardList[first].img !== cardList[second].img) {
+      setTimeout(() => {
+        setOpenCards([]);
+        setClearedCards(clearedCards?.filter((id) => {
+          if (!openCards?.includes(id)) return id;
+        }))
+      }, 800)
+    }
+  }
+
+  const handleClickOnCard = (index) => {
+    if (openCards?.includes(index)) return;
+    if (openCards?.length <= 1) {
+      setOpenCards(prev => ([...prev, index]));
+      setClearedCards(prev => ([...prev, index]))
+    } else {
+      setOpenCards([]);
+    }
+  }
+
+  useEffect(() => {
+    if (openCards?.length === 2) {
+      matchCards();
+    }
+  }, [openCards])
+
+  useEffect(() => {
+    if (uniqueCards?.length)
+      shuffleCards([...uniqueCards, ...uniqueCards])
+  }, [])
 
   return (
     <div className='card-container' >
       <div className='cards' >
-        { shuffleCards([...uniqueCards, ...uniqueCards]).map(({img}, index) => <div key={index} className="card">
-          <QuestionMarkIcon fontSize='large' style={{ color: '#193A81' }} />
-          {/* <img width={100} height={100} src={img} alt="" /> */}
+        {cardList.map(({ img }, index) => <div key={index} className="card" onClick={() => handleClickOnCard(index)} >
+          {
+            clearedCards.includes(index) ?
+              <img width={100} height={100} src={img} alt="" /> :
+              <QuestionMarkIcon fontSize='large' style={{ color: '#193A81' }} />
+          }
         </div>)}
       </div>
     </div>
